ac987013dda857f0b1e1f999be6b0e775c5b6e28
lctuan0807 <tuan.le@readytech.io>
2022-07-05 22:21:31 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fuchsia: colors.fuchsia
+      }
+    },
   },
   plugins: [],
 }
